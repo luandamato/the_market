b8550222bfd7081b46abc5c542d26e9e18a2c9fd
luandamato <luandamato@gmail.com>
2021-09-12 00:18:01 -0300

    bd

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
 module.exports = {
+    // dialect: 'mysql',
+    // host: 'localhost',
+    // username: 'root',
+    // password: '12345',
+    // database: 'TheMarket',
+    // define:{
+    //     timestamp: true,
+    //     underscored: true,
+    // },
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '12345',
-    database: 'TheMarket',
+    host: 'us-cdbr-east-04.cleardb.com',
+    username: 'bc0881e063281c',
+    password: 'f8e4f428',
+    database: 'heroku_7f203df84d4c27d',
     define:{
         timestamp: true,
         underscored: true,
